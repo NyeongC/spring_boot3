d6a0ad7cc0001888e8b0830fe009652d2c7b4c29
NyeongC <cjfsud23@naver.com>
2023-12-01 20:39:28 +0900

    한글깨짐 설정 application.properties

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,7 +11,14 @@ public class FirstController {
     // RestCotroller 와 Controller 뷰템플릿에 차이가 있는듯
     
     @GetMapping("/hi")
-    public String hello(){
+    public String niceToMertYou(Model model){
+        model.addAttribute("username","Choi Cheol Nyeong");
         return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","choi_nyeong");
+        return "goodbye";
     }
 }
