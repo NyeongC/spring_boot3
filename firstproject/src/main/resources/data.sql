e48ec410ea2eb26d1353606d4dde2a34310101c0
NyeongC <cjfsud23@naver.com>
2023-12-10 16:22:33 +0900

    JPA 로깅, DB 유니크 URL, ID값 자동생성

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO article(id, title, content) values (5, '가가가가', '1111');
-INSERT INTO article(id, title, content) values (6, '나나나나', '2222');
-INSERT INTO article(id, title, content) values (7, '다다다다', '3333');
+INSERT INTO article(title, content) values ('가가가가', '1111');
+INSERT INTO article(title, content) values ('나나나나', '2222');
+INSERT INTO article(title, content) values ('다다다다', '3333');
 
-INSERT INTO member(id, email, password) values (5, 'cjfsud23@naver.com', '1111');
-INSERT INTO member(id, email, password) values (6, 'cnchoi@naver.com', '2222');
-INSERT INTO member(id, email, password) values (7, 'kdtoaj14@naver.com', '3333');
+INSERT INTO member(email, password) values ('cjfsud23@naver.com', '1111');
+INSERT INTO member(email, password) values ('cnchoi@naver.com', '2222');
+INSERT INTO member(email, password) values ('kdtoaj14@naver.com', '3333');
