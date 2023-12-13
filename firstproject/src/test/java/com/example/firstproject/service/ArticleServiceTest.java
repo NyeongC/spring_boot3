242cc7a10ffa50ab44d8ea9e14d39cda17bc1574
NyeongC <cjfsud23@naver.com>
2023-12-13 23:34:31 +0900

    전체 리스트 조회 테스트 코드 작성

---
+++ firstproject/src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.entity.Article;
+import jakarta.persistence.Access;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // 해당 클래스를 스프링 부트와 연동해 통합테스트 수행 -> 스프링 부트가 관리하는 다양한 객체 주입받기 가능
+class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+        // 1. 예상 데이터
+        List<Article> articles = articleService.index();
+        // 2. 실제 데이터
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a,b,c));
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
+
+    }
+}
