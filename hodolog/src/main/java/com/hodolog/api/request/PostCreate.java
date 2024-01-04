baa0658801eed8f5153b824c56299aa78a9d4e71
NyeongC <cjfsud23@naver.com>
2024-01-04 20:40:52 +0900

    objectMapper.writeValueAsString / DTO -> Json 변환

--- hodolog/src/main/java/com/hodolog/api/request/PostCreate.java
+++ hodolog/src/main/java/com/hodolog/api/request/PostCreate.java
@@ -1,6 +1,7 @@
 package com.hodolog.api.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @ToString
 @Setter
 @Getter
+@AllArgsConstructor
 public class PostCreate {
 
     @NotBlank
--- hodolog/src/test/java/com/hodolog/api/controller/PostControllerTest.java
+++ hodolog/src/test/java/com/hodolog/api/controller/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.hodolog.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hodolog.api.domain.Post;
 import com.hodolog.api.repository.PostRepository;
+import com.hodolog.api.request.PostCreate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,10 +48,19 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 DB에 값이 저장된다.")
     void test3() throws Exception {
+
+        // given
+        PostCreate request = new PostCreate("제목입니다.","내용입니다.");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String request_json = objectMapper.writeValueAsString(request);
+
+        System.out.println("request_json = " + request_json);
+
         // when
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"제목입니다.\",\"content\": \"내용입니다.\"}")
+                        .content(request_json)
+                        //.content("{\"title\": \"제목입니다.\",\"content\": \"내용입니다.\"}") 보기에도 불편하고 짜침
                         //.content("{\"title\": \"\",\"content\": \"내용입니다.\"}")
                 )
                 .andExpect(status().isOk())
