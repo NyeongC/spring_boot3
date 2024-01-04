bc63dadc0bab4a5c90b3e7f65ebc5fdd6e794695
NyeongC <cjfsud23@naver.com>
2024-01-04 20:49:02 +0900

    @Builder 사용

--- hodolog/src/main/java/com/hodolog/api/request/PostCreate.java
+++ hodolog/src/main/java/com/hodolog/api/request/PostCreate.java
@@ -1,15 +1,13 @@
 package com.hodolog.api.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @ToString
 @Setter
 @Getter
 @AllArgsConstructor
+@Builder
 public class PostCreate {
 
     @NotBlank
