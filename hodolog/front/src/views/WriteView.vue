e703535790ca2e4c2b1c12c0400536bd9702cf71
NyeongC <cjfsud23@naver.com>
2024-01-16 23:32:34 +0900

    axios 추가, 글작성 api 호출 작성

--- hodolog/front/src/views/WriteView.vue
+++ hodolog/front/src/views/WriteView.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
-import {ref} from "vue";
-const count = ref(0)
+import {ref} from "vue"
+import axios from 'axios';
+
+const title = ref("")
+const content = ref("")
+
+const write = function (){
+  //alert(title + " / "+ content)
+  axios.post("http://localhost:8080/posts",
+      {
+        title: title.value,
+        content: content.value
+      })
+
+}
 </script>
 
 <template>
-  <p>안녕하세요</p>
-  <button @click="count +=1">버튼을 눌러주세요</button>
-  <p>{{count}}</p>
+  <div class="mt-2">
+  <el-input v-model="title" type="text" placeholder="제목을 입력하세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성완료</el-button>
+  </div>
+
 </template>
 
 <style scoped>
