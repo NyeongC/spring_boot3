13fcf1e86fb2829037ab6bea6fd389cf742a59f4
NyeongC <cjfsud23@naver.com>
2024-02-09 14:00:46 +0900

    Intercepter로 인증 처리해보기

---
+++ hodolog/src/main/java/com/hodolog/api/config/AuthInterCepter.java
@@ -0,0 +1,34 @@
+package com.hodolog.api.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class AuthInterCepter implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //return HandlerInterceptor.super.preHandle(request, response, handler);
+        log.info("preHandle");
+
+        String accesToken = request.getParameter("accessToken");
+        if(accesToken !=null && accesToken.equals("ccn")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        log.info("afterCompletion");
+    }
+}
--- hodolog/src/main/java/com/hodolog/api/config/WebConfig.java
+++ hodolog/src/main/java/com/hodolog/api/config/WebConfig.java
@@ -2,11 +2,17 @@ package com.hodolog.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new AuthInterCepter())
+                .excludePathPatterns("/foo"); // 특정 경로는 인터셉터 처리가 안되게 가능
+    }
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
