3af73fb4e1e9571f19c00db97a4de453cf9a2c02
NyeongC <cjfsud23@naver.com>
2024-02-11 17:59:41 +0900

    시큐리티 기본 설정

--- hodolog/src/main/java/com/hodolog/api/controller/AuthController.java
+++ hodolog/src/main/java/com/hodolog/api/controller/AuthController.java
@@ -24,6 +24,11 @@ public class AuthController {
         return "Hello";
     }
 
+    @GetMapping("/auth/login")
+    public String login(){
+        return "로그인 페이지입니다.";
+    }
+
     @PostMapping("/auth/signup")
     public void signup(@RequestBody Signup signup){
         authService.signup(signup);
