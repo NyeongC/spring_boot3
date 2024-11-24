680ebf57c582706293672e682dbcd061a9373575
NyeongC <cjfsud23@naver.com>
2024-11-24 11:16:33 +0900

    master 테스트 코드 delete

--- shin_spring/springboot-developer/src/main/java/me/choinyeong/springbootdeveloper/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package me.choinyeong.springbootdeveloper.repository;
-
-import me.choinyeong.springbootdeveloper.dao.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
