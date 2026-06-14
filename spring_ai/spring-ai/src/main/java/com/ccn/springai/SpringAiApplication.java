db359c31ea11561f22fdd62e11a283f74f7482a1
NyeongC <cjfsud23@naver.com>
2026-06-14 22:54:38 +0900

    JPA Auditing 활성화

--- spring_ai/spring-ai/src/main/java/com/ccn/springai/SpringAiApplication.java
+++ spring_ai/spring-ai/src/main/java/com/ccn/springai/SpringAiApplication.java
@@ -2,7 +2,9 @@ package com.ccn.springai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringAiApplication {
 
