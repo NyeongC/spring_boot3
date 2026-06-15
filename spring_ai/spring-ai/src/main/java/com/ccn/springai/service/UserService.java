e7555d7dbc680e12a9d1e886794be9595996e280
NyeongC <cjfsud23@naver.com>
2026-06-15 20:52:27 +0900

    스프링 시큐리티 로그인, 회원가입 구현

---
+++ spring_ai/spring-ai/src/main/java/com/ccn/springai/service/UserService.java
@@ -0,0 +1,22 @@
+package com.ccn.springai.service;
+
+import com.ccn.springai.domain.User;
+import com.ccn.springai.dto.AddUserRequest;
+import com.ccn.springai.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
